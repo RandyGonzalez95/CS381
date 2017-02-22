878d3b476eedbec8c41cf002c2c57cf907f1d8b4
Randy Gonzalez <randygonzalez@nevada.unr.edu>
2017-02-21 22:29:01 -0800

    Added AS1

---
+++ OgreTutorial3/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lOgreMain -lOgreTerrain -lOgreOverlay -lOIS -lboost_system
+
